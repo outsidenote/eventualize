4b3f2ff18a814030679601237d69593076c4e5d5
Bnaya Eshet <bnayaes@payoneer.com>
2023-12-11 13:16:18 +0200

    fix(SQLServerStorageAdapterTests): use xunit ctor for world context

--- Tests/Eventualize.Core.IntegrationTests/StorageAdapterTests/SQLServerStorageAdapterTests/SQLServerStorageAdapterTests.cs
+++ Tests/Eventualize.Core.IntegrationTests/StorageAdapterTests/SQLServerStorageAdapterTests/SQLServerStorageAdapterTests.cs
@@ -2,12 +2,12 @@ using CoreTests.StorageAdapterTests.SQLServerStorageAdapterTests.TestQueries;
 using Eventualize.Core;
 using Eventualize.Core.Tests;
 using Xunit.Abstractions;
-using static Eventualize.Core.Tests.TestContext;
 using static Eventualize.Core.Tests.TestHelper;
 
 
 
 namespace CoreTests.StorageAdapterTests.SQLServerStorageAdapterTests;
+
 
 public sealed class SQLServerStorageAdapterTests : IDisposable
 {
@@ -16,9 +16,13 @@ public sealed class SQLServerStorageAdapterTests : IDisposable
     private readonly ITestOutputHelper _output;
 #pragma warning restore S4487 // Unread "private" fields should be removed
 
+    private readonly SQLServerAdapterTestWorld _world;
+
     public SQLServerStorageAdapterTests(ITestOutputHelper output)
     {
         _output = output;
+
+        _world = new SQLServerAdapterTestWorld();
         BeforeEach().Wait();
     }
 
@@ -26,24 +30,19 @@ public sealed class SQLServerStorageAdapterTests : IDisposable
 
     private async Task BeforeEach()
     {
-        var world = new SQLServerAdapterTestWorld();
-        var name = TestName;
-        Worlds.Add(name, world);
-        await world.StorageAdapter.Init();
-        await world.StorageAdapter.CreateTestEnvironment();
+        await _world.StorageAdapter.Init();
+        await _world.StorageAdapter.CreateTestEnvironment();
     }
 
     private async Task AfterEach()
     {
-        var world = GetWorld();
-        await world.StorageAdapter.DestroyTestEnvironment();
+        await _world.StorageAdapter.DestroyTestEnvironment();
     }
 
     [Fact]
     public void SQLStorageAdapter_WhenCreatingTestEnvironment_Succeed()
     {
-        var world = GetWorld();
-        var command = AssertEnvironmentWasCreated.GetSqlCommand(world);
+        var command = AssertEnvironmentWasCreated.GetSqlCommand(_world);
         var reader = command.ExecuteReader();
         reader.Read();
         bool isEnvExist = reader.GetBoolean(0);
@@ -53,45 +52,40 @@ public sealed class SQLServerStorageAdapterTests : IDisposable
     [Fact]
     public async Task SQLStorageAdapter_WhenStoringAggregateWithPendingEventsWithoutSnapshot_Succeed()
     {
-        var world = GetWorld();
         var aggregate = await PrepareAggregateWithPendingEvents();
-        await world.StorageAdapter.SaveAsync(aggregate, false);
-        AssertAggregateStoredSuccessfully.assert(world, aggregate, false);
+        await _world.StorageAdapter.SaveAsync(aggregate, false);
+        AssertAggregateStoredSuccessfully.assert(_world, aggregate, false);
     }
 
     [Fact]
     public async Task SQLStorageAdapter_WhenStoringAggregateWithPendingEventsWithSnapshot_Succeed()
     {
-        var world = GetWorld();
         var aggregate = await PrepareAggregateWithPendingEvents();
-        await world.StorageAdapter.SaveAsync(aggregate, true);
-        AssertAggregateStoredSuccessfully.assert(world, aggregate, true);
+        await _world.StorageAdapter.SaveAsync(aggregate, true);
+        AssertAggregateStoredSuccessfully.assert(_world, aggregate, true);
     }
 
     [Fact]
     public async Task SQLStorageAdapter_WhenStoringAggregateWithStaleState_ThrowException()
     {
-        var world = GetWorld();
         var aggregate = await PrepareAggregateWithPendingEvents();
-        await world.StorageAdapter.SaveAsync(aggregate, true);
-        await Assert.ThrowsAsync<OCCException<TestState>>(async () => await world.StorageAdapter.SaveAsync(aggregate, true));
+        await _world.StorageAdapter.SaveAsync(aggregate, true);
+        await Assert.ThrowsAsync<OCCException<TestState>>(async () => await _world.StorageAdapter.SaveAsync(aggregate, true));
     }
 
     [Fact(Skip = "not active")]
     public async Task SQLStorageAdapter_WhenStoringAggregateWithFutureState_ThrowException()
     {
-        var world = GetWorld();
         var aggregate = await PrepareAggregateWithPendingEvents();
         var aggregate2 = new Aggregate<TestState>(aggregate.AggregateType, aggregate.Id, aggregate.MinEventsBetweenSnapshots, aggregate.PendingEvents);
-        await Assert.ThrowsAsync<OCCException<TestState>>(async () => await world.StorageAdapter.SaveAsync(aggregate2, true));
+        await Assert.ThrowsAsync<OCCException<TestState>>(async () => await _world.StorageAdapter.SaveAsync(aggregate2, true));
     }
 
     [Fact]
     public async Task SQLStorageAdapter_WhenGettingLastSnapshotId_Succeed()
     {
-        var world = GetWorld();
-        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(world.StorageAdapter);
-        var latestSnapshotSequenceId = await world.StorageAdapter.GetLastSequenceIdAsync(aggregate);
+        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(_world.StorageAdapter);
+        var latestSnapshotSequenceId = await _world.StorageAdapter.GetLastSequenceIdAsync(aggregate);
         var expectedSequenceId = aggregate.LastStoredSequenceId + aggregate.PendingEvents.Count;
         Assert.Equal(expectedSequenceId, latestSnapshotSequenceId);
     }
@@ -99,9 +93,8 @@ public sealed class SQLServerStorageAdapterTests : IDisposable
     [Fact]
     public async Task SQLStorageAdapter_WhenGettingLatestSnapshot_Succeed()
     {
-        var world = GetWorld();
-        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(world.StorageAdapter);
-        var latestSnapshot = await world.StorageAdapter.TryGetSnapshotAsync<TestState>(aggregate.AggregateType.Name, aggregate.Id);
+        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(_world.StorageAdapter);
+        var latestSnapshot = await _world.StorageAdapter.TryGetSnapshotAsync<TestState>(aggregate.AggregateType.Name, aggregate.Id);
         Assert.NotNull(latestSnapshot);
         Assert.Equal(aggregate.State, latestSnapshot.Snapshot);
         Assert.Equal(aggregate.LastStoredSequenceId + aggregate.PendingEvents.Count, latestSnapshot.SnapshotSequenceId);
@@ -110,20 +103,9 @@ public sealed class SQLServerStorageAdapterTests : IDisposable
     [Fact]
     public async Task SQLStorageAdapter_WhenGettingStoredEvents_Succeed()
     {
-        var world = GetWorld();
-        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(world.StorageAdapter);
-        var events = await world.StorageAdapter.GetAsync(aggregate.AggregateType.Name, aggregate.Id, aggregate.LastStoredSequenceId + 1);
+        var aggregate = await SQLServerStorageAdapterTestsSteps.StoreAggregateTwice(_world.StorageAdapter);
+        var events = await _world.StorageAdapter.GetAsync(aggregate.AggregateType.Name, aggregate.Id, aggregate.LastStoredSequenceId + 1);
         Assert.NotNull(events);
         SQLServerStorageAdapterTestsSteps.AssertEventsAreEqual(events, aggregate.PendingEvents);
-
     }
-
-
-    private SQLServerAdapterTestWorld GetWorld()
-    {
-        if (!Worlds.TryGetValue(TestName, out var world))
-            throw new KeyNotFoundException();
-        return world;
-    }
-
 }
