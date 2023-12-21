2133f5e6501d9f1643e65c492ac71dfa94b09731
Bnaya Eshet <bnayaes@payoneer.com>
2023-12-21 10:05:58 +0200

    ci(docker-compose)

---
+++ Adapters/Store/Postgres/Eventualize.Core.Adapters.PostgresStore.Adapters/QueryTemplatesFactory.cs
@@ -0,0 +1,78 @@
+﻿namespace Eventualize.Core.Adapters.SqlStore;
+
+// TODO: [bnaya 2023-12-19] all parameters and field should be driven from nameof or const
+
+internal static class QueryTemplatesFactory
+{
+    public static EventualizeAdapterQueryTemplates Create(EventualizeStorageContext storageContext)
+    {
+        return new EventualizeAdapterQueryTemplates
+        {
+            GetLastSnapshotSequenceId = $"""
+                SELECT MAX(sequence_id)
+                    FROM {storageContext}snapshot
+                    WHERE domain = 'default'
+                        AND aggregate_type = @{nameof(AggregateParameter.Type)}
+                        AND aggregate_id = @{nameof(AggregateParameter.Id)}
+                """,
+            TryGetSnapshot = $"""
+                SELECT json_data as {nameof(EventualizeStoredSnapshotData<object>.Snapshot)}, sequence_id as {nameof(EventualizeStoredSnapshotData<object>.SnapshotSequenceId)}
+                FROM {storageContext}snapshot
+                WHERE domain = 'default'
+                    AND aggregate_type = @{nameof(AggregateParameter.Type)}
+                    AND aggregate_id = @{nameof(AggregateParameter.Id)}
+                ORDER BY sequence_id DESC
+                OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY;
+                """,
+            GetEvents = $"""
+                SELECT
+                    event_type as {nameof(EventualizeEvent.EventType)},
+                    captured_at as {nameof(EventualizeEvent.CapturedAt)},
+                    captured_by as {nameof(EventualizeEvent.CapturedBy)},
+                    json_data as {nameof(EventualizeEvent.JsonData)},
+                    stored_at as {nameof(EventualizeEvent.StoredAt)}                    
+                FROM {storageContext}event
+                WHERE domain = 'default'
+                    AND aggregate_type = @{nameof(AggregateParameter.Type)}
+                    AND aggregate_id = @{nameof(AggregateParameter.Id)}
+                    and sequence_id >= @{nameof(AggregateSequenceParameter.Sequence)};
+                """,
+            // take a look at https://www.learndapper.com/saving-data/insert
+            Save = $"""
+                    INSERT INTO {storageContext}event (
+                        aggregate_id,
+                        aggregate_type, 
+                        event_type, 
+                        sequence_id,
+                        json_data,
+                        captured_by,
+                        captured_at, 
+                        domain) 
+                    VALUES (
+                        @{nameof(AggregateSaveParameter.AggregateId)}, 
+                        @{nameof(AggregateSaveParameter.AggregateType)}, 
+                        @{nameof(AggregateSaveParameter.EventType)}, 
+                        @{nameof(AggregateSaveParameter.Sequence)}, 
+                        @{nameof(AggregateSaveParameter.Payload)},
+                        @{nameof(AggregateSaveParameter.CapturedBy)},
+                        @{nameof(AggregateSaveParameter.CapturedAt)}, 
+                        @{nameof(AggregateSaveParameter.Domain)})
+                    """,
+            SaveSnapshot = $"""
+            INSERT INTO {storageContext}snapshot (
+                        aggregate_id,
+                        aggregate_type, 
+                        sequence_id,
+                        json_data,
+                        domain)
+            VALUES (
+                        @{nameof(SnapshotSaveParameter.AggregateId)},
+                        @{nameof(SnapshotSaveParameter.AggregateType)},
+                        @{nameof(SnapshotSaveParameter.Sequence)},
+                        @{nameof(SnapshotSaveParameter.Payload)},
+                        @{nameof(SnapshotSaveParameter.Domain)})
+            """
+
+        };
+    }
+}
---
+++ docker/OTEL/README.md
@@ -0,0 +1,87 @@
+## Event Sourcing Backbone + Redis, Jaeger, Prometheus & Grafana
+
+Project structure:
+
+```txt
+.
+├── compose.yaml
+├── redis
+├── jaeger
+├── grafana
+│   └── datasource.yml
+├── prometheus
+│   └── prometheus.yml
+└── README.md
+```
+
+[_compose.yaml_](compose.yaml)
+
+```yml
+services:
+  redis:
+    image: redislabs/rejson
+    ...
+    ports:
+      - 6379:6379
+  jaeger:
+    image: jaegertracing/opentelemetry-all-in-one
+    ...
+    ports:
+      - 16686:16686
+      - 4318:4318
+      - 4317:4317
+prometheus:
+    image: prom/prometheus
+    ...
+    ports:
+      - 9090:9090
+  grafana:
+    image: grafana/grafana
+    ...
+    ports:
+      - 3000:3000
+```
+
+The compose file defines a stack with two services `prometheus` and `grafana`.
+When deploying the stack, docker compose maps port the default ports for each service to the equivalent ports on the host in order to inspect easier the web interface of each service.
+Make sure the ports 9090 and 3000 on the host are not already in use.
+
+## Deploy with docker compose
+
+```bash
+$ docker compose up -d
+Creating network "event-sourcing-backbone_default" with the default driver
+Creating volume "event-sourcing-backbone_prom_data" with default driver
+...
+Creating redis    ... done
+Creating jaeger    ... done
+Creating grafana    ... done
+Creating prometheus ... done
+Attaching to prometheus, grafana
+
+```
+
+## Expected result
+
+Listing containers must show two containers running and the port mapping as below:
+
+```bash
+$ docker ps
+CONTAINER ID        IMAGE                                     COMMAND                  CREATED             STATUS              PORTS                    NAMES
+xsdec63w814f        redislabs/rejson                          "..."                    8 minutes ago       Up 8 minutes        0.0.0.0:6379->6379/tcp   redis
+abdes63s814f        jaegertracing/opentelemetry-all-in-one    "..."                    8 minutes ago       Up 8 minutes        0.0.0.0:...              jaeger
+dbdec637814f        prom/prometheus                           "/bin/prometheus --c…"   8 minutes ago       Up 8 minutes        0.0.0.0:9090->9090/tcp   prometheus                      
+79f667cb7dc2        grafana/grafana                           "/run.sh"                8 minutes ago       Up 8 minutes        0.0.0.0:3000->3000/tcp   grafana
+```
+
+Navigate to `http://localhost:3000` in your web browser and use the login credentials specified in the compose file to access Grafana. It is already configured with prometheus as the default datasource.
+
+![page](output.jpg)
+
+Navigate to `http://localhost:9090` in your web browser to access directly the web interface of prometheus.
+
+Stop and remove the containers. Use `-v` to remove the volumes if looking to erase all data.
+
+```bash
+$ docker compose down -v
+```
