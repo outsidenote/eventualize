ab306e37e6e895e73c55cd543f21a61ffb39432d
Bnaya Eshet <bnayaes@payoneer.com>
2024-01-22 13:28:15 +0200

    refactor

---
+++ Samples/EvDb.Scenes/Generation.Instructions/School/Views/StatsView.cs
@@ -0,0 +1,29 @@
+﻿using EvDb.Core;
+using EvDb.Scenes;
+
+namespace EvDb.UnitTests;
+
+[EvDbView<Stats, ISchoolEventBundle>(PropertyName = "All")]
+internal partial class StatsView
+{
+    protected override Stats DefaultState { get; } = new Stats(0, 0);
+
+    public override int MinEventsBetweenSnapshots => 5;
+
+    #region Fold
+
+    protected override Stats Fold(
+        Stats state,
+        StudentReceivedGradeEvent receivedGrade,
+        IEvDbEventMeta meta)
+    {
+        var result = state with
+        {
+            Count = state.Count + 1,
+            Sum = state.Sum + receivedGrade.Grade,
+        };
+        return result;
+    }
+
+    #endregion // Fold
+}
--- Samples/EvDb.Scenes/Generation.Instructions/School/Views/StudentStatsView.cs
+++ Samples/EvDb.Scenes/Generation.Instructions/School/Views/StudentStatsView.cs
@@ -51,28 +51,3 @@ internal partial class StudentStatsView
 
     #endregion // Fold
 }
-
-[EvDbView<Stats, ISchoolEventBundle>(PropertyName = "All")]
-internal partial class StatsView
-{
-    protected override Stats DefaultState { get; } = new Stats(0, 0);
-
-    public override int MinEventsBetweenSnapshots => 5;
-
-    #region Fold
-
-    protected override Stats Fold(
-        Stats state,
-        StudentReceivedGradeEvent receivedGrade,
-        IEvDbEventMeta meta)
-    {
-        var result = state with
-        {
-            Count = state.Count + 1,
-            Sum = state.Sum + receivedGrade.Grade,
-        };
-        return result;
-    }
-
-    #endregion // Fold
-}
