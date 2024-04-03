4bd6b1f3d003fcdaa9babe125e69a44f1c4ac051
Bnaya & Payoneer <144127531+bnaya-payoneer@users.noreply.github.com>
2024-04-03 16:39:42 +0300

    feat(Factory DI) (#135)
    Co-authored-by: Bnaya Eshet <bnaya-payoneer>

--- Samples/EvDb.Samples.EvDbWebSample/Program.cs
+++ Samples/EvDb.Samples.EvDbWebSample/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEvDbIssueStreamFactory();
+builder.Services.AddEvDbSchoolStreamFactory();
 
 var app = builder.Build();
 
