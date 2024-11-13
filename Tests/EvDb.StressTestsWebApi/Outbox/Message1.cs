e20ee2100f6cd56b42e86d75aad7f65127154eec
Bnaya Eshet <bnayaes@payoneer.com>
2024-11-13 14:00:19 +0200

    fix merge

--- Tests/EvDb.StressTestsWebApi/Outbox/Message1.cs
+++ Tests/EvDb.StressTestsWebApi/Outbox/Message1.cs
@@ -4,7 +4,7 @@ using EvDb.Core;
 
 namespace EvDb.StressTestsWebApi.Outbox;
 
-[EvDbDefinePayload("message-1")]
+[EvDbDefineMessagePayload("message-1")]
 [EvDbAttachChannel("Channel1")]
 [EvDbAttachChannel("Channel2")]
 public partial record Message1(double Value);
--- Tests/EvDb.StressTestsWebApi/Outbox/Message2.cs
+++ Tests/EvDb.StressTestsWebApi/Outbox/Message2.cs
@@ -2,7 +2,7 @@
 
 namespace EvDb.StressTestsWebApi.Outbox;
 
-[EvDbDefinePayload("message-2")]
+[EvDbDefineMessagePayload("message-2")]
 [EvDbAttachChannel("Channel1")]
 [EvDbAttachChannel("Channel3")]
 public partial record Message2(double Value);
