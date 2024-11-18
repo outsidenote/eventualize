e3247fe968871174ddedd86fb2a4ab23ae086bf4
Mor Eliyahu <morel@payoneer.com>
2024-11-18 10:34:47 +0200

    add EmbeddedSchemaSerializer

---
+++ Tests/EvDb.StressTestsWebApi/Outbox/EmbededSchemaSerializer.cs
@@ -0,0 +1,41 @@
+﻿using Avro.File;
+using Avro.Generic;
+using Avro.Specific;
+using System.Runtime.Intrinsics.Arm;
+using EvDb.Core;
+
+namespace EvDb.StressTestsWebApi.Outbox
+{
+    public class EmbeddedSchemaSerializer : IEvDbOutboxSerializer
+    {
+        private const int maxResultCapacity = 1024 * 5;
+        string IEvDbOutboxSerializer.Name => throw new NotImplementedException();
+
+        byte[] IEvDbOutboxSerializer.Serialize<T>(EvDbChannelName channel, EvDbShardName shardName, T payload)
+        {
+            //   private async Task<byte[]> ToBytesAsync<TPayload>(TPayload payLoad) where TPayload : AvroBase
+
+            if (payload is not ISpecificRecord record)
+            { 
+                throw new NotSupportedException();
+            }
+
+            DatumWriter<T> writer = new SpecificDatumWriter<T>(record.Schema);
+            using var output = new MemoryStream(maxResultCapacity);
+
+            var specDataWriter = DataFileWriter<T>.OpenWriter(writer, output);
+            specDataWriter.Append(payload);
+            specDataWriter.Flush();
+            specDataWriter.Close();
+
+            var result = output.ToArray();
+
+            return result;
+        }
+
+        bool IEvDbOutboxSerializer.ShouldSerialize<T>(EvDbChannelName channel, EvDbShardName shardName, T payload)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tests/EvDb.StressTestsWebApi/Outbox/Message2.cs
+++ Tests/EvDb.StressTestsWebApi/Outbox/Message2.cs
@@ -1,8 +1,39 @@
-﻿using EvDb.Core;
+﻿using Avro;
+using Avro.Specific;
+using EvDb.Core;
 
 namespace EvDb.StressTestsWebApi.Outbox;
 
 [EvDbDefineMessagePayload("message-2")]
 [EvDbAttachChannel("Channel1")]
 [EvDbAttachChannel("Channel3")]
-public partial record Message2(double Value);
+public partial record Message2 : ISpecificRecord
+{
+    public double Value { get; set; }
+
+    Schema ISpecificRecord.Schema => Avro.Schema.Parse(
+        """
+        {
+            'name': 'Message2',
+        	'type': 'record',
+        	'namespace': 'EvDb.StressTestsWebApi.Outbox',
+        	'fields': 
+        	[
+               {
+                    'name': 'Value',
+        			'type': 'double'
+                }
+        	]
+        }
+        """);
+
+    object ISpecificRecord.Get(int fieldPos)
+    {
+        return Value;
+    }
+
+    void ISpecificRecord.Put(int fieldPos, object fieldValue)
+    {
+        Value = (double)fieldValue;
+    }
+}
