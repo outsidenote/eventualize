48e9d2e8dbe074c53bb29dc1b274b2e8f1e0dd4d
PayOutSide <141309745+PayOutSide@users.noreply.github.com>
2024-11-19 08:33:17 +0200

    Feature/add embedded schema serializer (#170)
    * add EmbeddedSchemaSerializer
    
    * Add new serializers and update outbox handling
    
    * Added `Microsoft.Extensions.Caching.Memory` version `8.0.1` and `System.Text.Json` version `8.0.5` as package references in `EvDb.StressTestsWebApi.csproj`.
    * Implemented the `Name` property in `EmbeddedSchemaSerializer` to return the name of the class instead of throwing a `NotImplementedException`.
    * Implemented the `ShouldSerialize` method in `EmbeddedSchemaSerializer` to return `true` if the `shardName` is `OutboxShards.Table1`.
    * Updated `StressTestOutbox` to use `EmbeddedSchemaSerializer` for outbox serialization instead of the commented-out `AvroSerializer` and `PrefixSerializer`.
    * Modified the `ProduceOutboxMessages` method in `StressTestOutbox` to use the correct parameter name `outbox` and to add messages with object initializers instead of constructor parameters.
    
    * merge
    
    ---------
    
    Co-authored-by: Bnaya Eshet <bnayaes@payoneer.com>

--- Tests/EvDb.StressTestsWebApi/Outbox/Message1.cs
+++ Tests/EvDb.StressTestsWebApi/Outbox/Message1.cs
@@ -1,5 +1,7 @@
 ﻿// Ignore Spelling: Channel
 
+using Avro;
+using Avro.Specific;
 using EvDb.Core;
 
 namespace EvDb.StressTestsWebApi.Outbox;
@@ -7,4 +9,33 @@ namespace EvDb.StressTestsWebApi.Outbox;
 [EvDbDefineMessagePayload("message-1")]
 [EvDbAttachChannel("Channel1")]
 [EvDbAttachChannel("Channel2")]
-public partial record Message1(double Value);
+public partial record Message1() : ISpecificRecord
+{
+    public double Amount { get; set; }
+
+    Schema ISpecificRecord.Schema => Avro.Schema.Parse(
+        """
+        {
+            'name': 'Message1',
+        	'type': 'record',
+        	'namespace': 'EvDb.StressTestsWebApi.Outbox',
+        	'fields': 
+        	[
+               {
+                    'name': 'Amount',
+        			'type': 'double'
+                }
+        	]
+        }
+        """);
+
+    object ISpecificRecord.Get(int fieldPos)
+    {
+        return Amount;
+    }
+
+    void ISpecificRecord.Put(int fieldPos, object fieldValue)
+    {
+        Amount = (double)fieldValue;
+    }
+}
