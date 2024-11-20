232bc8070ee49761df2161ece951e014cdb5e166
Bnaya Eshet <bnayaes@payoneer.com>
2024-11-20 16:32:40 +0200

    first stage

---
+++ StaticAnalysis/EvDb.Core.Analyzer/EvDbCoreAnalyzer.cs
@@ -0,0 +1,61 @@
+﻿using EvDb.Core.Analyzer;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace EvDb.Core
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class EvDbCoreAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "EvDb.Core.Analyzer";
+        private const string Category = "EvDb";
+        private static ImmutableHashSet<string> TARGET_ATTRIBUTES =
+            ImmutableHashSet.CreateRange([
+                "EvDbDefineEventPayloadAttribute",
+                "EvDbDefineMessagePayloadAttribute",
+                "EvDbEventTypesAttribute",
+                "EvDbViewTypeAttribute",
+                "EvDbOutboxAttribute",
+                "EvDbStreamFactoryAttribute"]);
+
+        private static readonly DiagnosticDescriptor PartialIsMissingRule = new DiagnosticDescriptor(
+                                                        DiagnosticId,
+                                                        "Partial class is missing",
+                                                        "Type must be partial", 
+                                                        Category,
+                                                        DiagnosticSeverity.Error,
+                                                        isEnabledByDefault: true,
+                                                        description: "Source code generator needs a partial type");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(PartialIsMissingRule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var attrs = namedTypeSymbol.GetAttributes();
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (!attrs.Any(a => TARGET_ATTRIBUTES.Contains(a.AttributeClass.Name)))
+                return;
+
+            if (!namedTypeSymbol.IsPartial())
+                return;
+
+            namedTypeSymbol.CreateDiagnostic(PartialIsMissingRule);            
+        }
+    }
+}
---
+++ StaticAnalysis/EvDb.Core.CodeFixes/EvDbCoreCodeFixProvider.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EvDb.Core
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(EvDbCoreCodeFixProvider)), Shared]
+    public class EvDbCoreCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(EvDbCoreAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
+                                             .ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: "Fix EvDb partial type",
+                    createChangedSolution: c => 
+                    FixPartial(context.Document, declaration, c),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private async Task<Solution> FixPartial(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        {
+            // Compute new uppercase name.
+            var identifierToken = typeDecl.Identifier;
+            var newName = identifierToken.Text.ToUpperInvariant();
+
+            // Get the symbol representing the type to be renamed.
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+
+            // Produce a new solution that has all references to that type renamed, including the declaration.
+            var originalSolution = document.Project.Solution;
+            var optionSet = originalSolution.Workspace.Options;
+            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+
+            // Return the new solution with the now-uppercase type name.
+            var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            if (syntaxRoot == null)
+                return document.Project.Solution;
+
+            //// Check if the type already has the 'partial' modifier
+            //if (typeDecl.Modifiers.Any(modifier => modifier.IsKind(SyntaxKind.PartialKeyword)))
+            //{
+            //    // The type is already partial, no fix needed
+            //    return document.Project.Solution;
+            //}
+
+            // Add the 'partial' modifier to the type declaration
+            var updatedTypeDecl = typeDecl.WithModifiers(
+                typeDecl.Modifiers.Add(SyntaxFactory.Token(SyntaxKind.PartialKeyword))
+            );
+
+            // Replace the old node with the updated one
+            var updatedSyntaxRoot = syntaxRoot.ReplaceNode(typeDecl, updatedTypeDecl);
+
+            // Return the updated solution
+            var updatedDocument = document.WithSyntaxRoot(updatedSyntaxRoot);
+            return updatedDocument.Project.Solution;
+        }
+    }
+}
