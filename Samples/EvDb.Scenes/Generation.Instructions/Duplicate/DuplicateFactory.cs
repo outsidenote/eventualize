4f5a724fc61dd6f432fdc7d854e9d210443a0d08
Bnaya Eshet <bnayaes@payoneer.com>
2025-05-05 08:02:53 +0300

    no bug

---
+++ Samples/EvDb.Scenes/Generation.Instructions/Duplicate/DuplicateFactory.cs
@@ -0,0 +1,12 @@
+﻿using EvDb.Core;
+
+namespace EvDb.UnitTests;
+
+[EvDbAttachView<DuplicateView>]
+[EvDbStreamFactory<IEvDbSchoolStreamAdders, EvDbDuplicateOutbox>(
+                        "duplicate", "issue")]
+// Lifetime = ServiceLifetime.Scoped)]
+public partial class DuplicateFactory
+{
+}
+
---
+++ Samples/EvDb.Scenes/Generation.Instructions/Duplicate/EvDbDuplicateOutbox.cs
@@ -0,0 +1,20 @@
+﻿// Ignore Spelling: OutboxProducer Channel
+
+using EvDb.Core;
+using EvDb.Scenes;
+
+namespace EvDb.UnitTests;
+
+[EvDbAttachMessageType<AvgMessage>]
+[EvDbAttachMessageType<StudentPassedMessage>]
+[EvDbAttachMessageType<StudentFailedMessage>]
+[EvDbOutbox<DuplicateFactory>]
+public partial class EvDbDuplicateOutbox
+{
+    protected override void ProduceOutboxMessages(StudentReceivedGradeEvent payload, IEvDbEventMeta meta, EvDbDuplicateViews views, EvDbDuplicateOutboxContext outbox)
+    {
+        Stats state = views.Duplicate;
+        AvgMessage avg = new(state.Sum / (double)state.Count);
+        outbox.Add(avg);
+    }
+}
