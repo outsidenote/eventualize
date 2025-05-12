28094fb73fb4beb2130a6aa473c6ee84488d7b97
Bnaya Eshet Corp <144127531+bnaya-corp@users.noreply.github.com>
2025-05-12 20:29:35 +0300

    otel info on message (#213)
    * otel info on message
    
    * fix telemetry tracking

--- EvDb.Abstractions/Interfaces/Outbox/IEvDbMessageMeta.cs
+++ EvDb.Abstractions/Interfaces/Outbox/IEvDbMessageMeta.cs
@@ -1,7 +1,24 @@
 ﻿namespace EvDb.Core;
 
+/// <summary>
+/// Messsage metadata
+/// </summary>
 public interface IEvDbMessageMeta : IEvDbEventMeta
 {
+    /// <summary>
+    /// The message type
+    /// </summary>
     string MessageType { get; }
+    /// <summary>
+    /// A channel attached to the message
+    /// </summary>
     EvDbChannelName Channel { get; }
+    /// <summary>
+    /// OTEL trace
+    /// </summary>
+    public string? TraceId { get; }
+    /// <summary>
+    /// OTEL Span
+    /// </summary>
+    public string? SpanId { get; }
 }
--- Tests/EvDb.UnitTests/MessageRecordToMetadataTests.cs
+++ Tests/EvDb.UnitTests/MessageRecordToMetadataTests.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
 using EvDb.Adapters.Store.Internals;
+using System.Diagnostics;
 
 namespace EvDb.UnitTests;
 
@@ -85,7 +86,46 @@ public class MessageRecordToMetadataTests
         Assert.Equal(messageRecord.Offset, meta.StreamCursor.Offset);
         Assert.Equal(messageRecord.EventType, meta.EventType);
         Assert.Equal(messageRecord.Channel, meta.Channel);
-       // Assert.Equal(messageRecord.CapturedAt, meta.CapturedAt);
+        Assert.Equal(messageRecord.TraceId, meta.TraceId);
+        Assert.Equal(messageRecord.SpanId, meta.SpanId);
+        Assert.Equal(messageRecord.CapturedBy, meta.CapturedBy);
+    }
+
+    [Fact]
+    public void BsonMessageRecordToMetadataFromActivityTelemetry()
+    {
+        var messageRecord = new EvDbMessageRecord
+        {
+            Domain = "TestDomain",
+            Partition = "TestPartition",
+            StreamId = "TestStreamId",
+            Offset = 1,
+            EventType = "TestEventType",
+            Channel = "TestChannel",
+            MessageType = "TestMessageType",
+            SerializeType = "TestSerializeType",
+            Payload = Encoding.UTF8.GetBytes("TestPayload"),
+            CapturedBy = "TestCapturedBy",
+            CapturedAt = DateTimeOffset.UtcNow,
+        };
+
+        ActivitySource activitySource = new ActivitySource("Test");
+        var activity = new Activity("test-scope");
+        activity.Start();
+        Activity.Current = activity;
+        var doc = messageRecord.EvDbToBsonDocument("shard");
+        var meta = doc.ToMessageMeta();
+
+        _output.WriteLine($"MessageRecord: {messageRecord}");
+
+        Assert.Equal(messageRecord.Domain, meta.StreamCursor.Domain);
+        Assert.Equal(messageRecord.Partition, meta.StreamCursor.Partition);
+        Assert.Equal(messageRecord.StreamId, meta.StreamCursor.StreamId);
+        Assert.Equal(messageRecord.Offset, meta.StreamCursor.Offset);
+        Assert.Equal(messageRecord.EventType, meta.EventType);
+        Assert.Equal(messageRecord.Channel, meta.Channel);
+        Assert.Equal(Activity.Current?.TraceId.ToHexString(), meta.TraceId);
+        Assert.Equal(Activity.Current?.SpanId.ToHexString(), meta.SpanId);
         Assert.Equal(messageRecord.CapturedBy, meta.CapturedBy);
     }
 
