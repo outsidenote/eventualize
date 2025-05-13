006944571b35a3cdd2ee04a02bee2d6556672ce6
Bnaya Eshet <bnayaes@payoneer.com>
2025-05-13 08:26:21 +0300

    in mem

---
+++ Adapters/Store/InMem/EvDb.Adapters.Store.EvDbInMem/InMemUnit.cs
@@ -0,0 +1,13 @@
+﻿// Ignore Spelling: InMemory
+
+using EvDb.Core;
+using System.Collections.Immutable;
+
+namespace EvDb.Adapters.Store.InMemory;
+
+public record InMemoryUnit
+{
+    public required ImmutableList<EvDbEvent> Events { get; init; }
+    public required ImmutableList<EvDbMessage> Messages { get; init; }
+    public required ImmutableDictionary<EvDbViewAddress, EvDbStoredSnapshotData> Snapshots { get; init; }
+}
